17128feaaea40fe3d04855e4b7b5b925cc153e89
Glomberg <Bazz@bk.ru>
2015-03-05 09:25:41 +0300

    321

---
+++ www/js/zakaz.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+/////////////////////////////////////
+// КАЛЬКУЛЯТОР
+/////////////////////////////////////
+	$("#number").keyup("change", function(){
+		var number = $("#number").val();
+		$(".sum").text(number);
+		$(".sum").append(" руб.");
+	});
+});
